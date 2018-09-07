3c6c236f1fed7aa20656581fa2a87a5a7da9179a
GavrilyukAG <sashagavrilyuk97@gmail.com>
2018-09-07 14:56:36 +0300

    Revert "hw"
    This reverts commit 06d905ca857237ddbbb0048cc91f9069a0d272db.

--- source/sort.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const sort = (str) => {
-    var subStr = str.toLowerCase().split(' ');
-    return subStr.map((item) => {
-        item = item.split('').sort((a, b) => a.localeCompare(b)).join('');
-        return item.charAt(0).toUpperCase() + item.slice(1);
-    }).sort().join(' ');
-};
